c42fc22333458a2d9d62cd3f2dacb4e7f6a04f23
fastdaima <sivaramakrishnang25@gmail.com>
2024-06-23 23:35:46 +0530

    june 23

--- codeforces/1538_c_number_of_pairs.py
+++ codeforces/1538_c_number_of_pairs.py
@@ -5,16 +5,41 @@ from os import path
 
 def read_input(): 
     if path.exists("input.txt"): 
-        sys.stdin = open('input.txt', 'r')
-        sys.stdout = open('output.txt', 'r')
+        read = open('input.txt', 'r').readline
+        write = open('output.txt', 'w').write
     
-    input_ = sys.stdin.readline()
-    return input_
+    return read, write 
+
+read, write = read_input()
+
+def map_inp_var():
+    return (tuple(map(int, read().split())))
+
+def map_inp_arr():
+    return list(map(int, read().split()))
+
+w = int(read())
 
 
+for _ in range(w): 
+    n, l, r = map_inp_var()
+    # print(n,l,r)
+    a = map_inp_arr()
+    a = sorted(a)
 
-breakpoint()
-w = read_input() 
+    start = 0 
+    end = 0
+    count = 0
+    # print(a, '\t')
 
+    while(start<n):
+        n-=1
+        while(start<n and a[start]+a[n]<l):
+            start += 1
+        while(end<n and a[end]+a[n]<=r):
+            end += 1
 
-# sys.stdout.close()
+        count += min(end, n) - start 
+    # print(count)
+    write(str(count)+'\n')
+
